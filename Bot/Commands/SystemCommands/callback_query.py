0a56cd50b0393aeb60ffad2c6f9e46593783cf5b
Abdil <90824488+metatronslove@users.noreply.github.com>
2025-05-04 06:23:46 +0300

    Add files via upload

--- Bot/Commands/SystemCommands/callback_query.py
+++ Bot/Commands/SystemCommands/callback_query.py
@@ -134,7 +134,7 @@ async def handle_callback_query(update: Update, context: ContextTypes.DEFAULT_TY
         elif data.startswith("indian_square_"):
             row_sum = int(data[len("indian_square_"):])
             magic_square = MagicSquareGenerator()
-            square = magic_square.generate_magic_square( nigerian_magic_square(3, row_sum, 0, False, "indian")
+            square = magic_square.generate_magic_square(3, row_sum, 0, False, "indian")
             response = i18n.t("MAGICSQUARE_RESULT", language, number=row_sum, square=square["box"])
             commentary = await get_ai_commentary(response, language)
             if commentary:
