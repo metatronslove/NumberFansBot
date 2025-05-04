3a193f9ccb76e6c11e28ce0c0cef36b8cfd3f945
Abdil <90824488+metatronslove@users.noreply.github.com>
2025-05-04 06:12:43 +0300

    Add files via upload

--- Bot/Commands/UserCommands/__init__.py
+++ Bot/Commands/UserCommands/__init__.py
@@ -15,7 +15,6 @@ from .nutket import nutket_handle
 from .suggest_transliteration import suggest_transliteration_handle
 from .numerology import numerology_handle
 from .cancel import cancel_handle
-from .callback_query import set_language_handle, handle_callback_query
 from .huddam import get_huddam_conversation_handler  # Added
 from .bastet import get_bastet_conversation_handler  # Added
 from .abjad import get_abjad_conversation_handler  # Added
