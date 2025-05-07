8c3a5f9e269cea1ea0abc255e0291d75c022c50f
Abdil <90824488+metatronslove@users.noreply.github.com>
2025-05-07 22:02:19 +0300

    Add files via upload

--- Bot/Commands/UserCommands/__init__.py
+++ Bot/Commands/UserCommands/__init__.py
@@ -1,3 +1,8 @@
+from .huddam import get_huddam_conversation_handler  # Added
+from .bastet import get_bastet_conversation_handler  # Added
+from .abjad import get_abjad_conversation_handler  # Added
+from .unsur import get_unsur_conversation_handler
+from .payment import payment_handle, get_payment_handlers
 from .start import start_handle
 from .credits import credits_handle
 from .help import help_handle
@@ -5,17 +10,12 @@ from .magic_square import magic_square_handle
 from .convert_numbers import convert_numbers_handle
 from .transliterate import transliterate_handle
 from .language import language_handle
-from .payment import payment_handle, get_payment_handlers
 from .transliteration_history import transliteration_history_handle
 from .numerology_square import numerology_square_handle
 from .help_group_chat import help_group_chat_handle
 from .settings import settings_handle
 from .name import name_handle
-from .unsur import get_unsur_conversation_handler
 from .nutket import nutket_handle
 from .suggest_transliteration import suggest_transliteration_handle
 from .numerology import numerology_handle
 from .cancel import cancel_handle
-from .huddam import get_huddam_conversation_handler  # Added
-from .bastet import get_bastet_conversation_handler  # Added
-from .abjad import get_abjad_conversation_handler  # Added
