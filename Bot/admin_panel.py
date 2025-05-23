3ffe936df7232f71d8657cfc89de0f926803cd32
Abdil <90824488+metatronslove@users.noreply.github.com>
2025-05-23 15:17:35 +0300

    Add files via upload

--- Bot/admin_panel.py
+++ Bot/admin_panel.py
@@ -95,7 +95,8 @@ except Exception as e:
 	logger.error(f"Failed to schedule Telegram initialization: {str(e)}")
 	raise
 
-# Register handlersdef register_handlers():
+# Register handlers
+def register_handlers():
 	from .Commands.UserCommands.abjad import get_abjad_conversation_handler
 	from .Commands.UserCommands.bastet import get_bastet_conversation_handler
 	from .Commands.UserCommands.huddam import get_huddam_conversation_handler
